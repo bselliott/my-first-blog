8c15472a8e03d457c9d6563cdd46279a5421afcc
BrianElliott <bselliott1197@gmail.com>
2018-06-05 16:23:49 -0500

    Added view and tamplated for detailed blog post as well as css for the site

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import import Post
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+def post_list(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_list.html', {'posts': posts})
